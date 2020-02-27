a5e222330cf9d68dd0a5b4bd52361defe79cfa6b
mathtkang <51039577+mathtkang@users.noreply.github.com>
2020-02-27 14:11:32 +0900

    input.value에 여러개의 글자가 동시에 나옴/AC버튼 클릭하면 input=0 되도록
    200227

--- javascript.js
+++ javascript.js
@@ -1,18 +1,32 @@
 var td = document.querySelectorAll('td')
-//클로저
-//이벤트가 실행되면 자동으로 파라미터가 생김 => event
-
 for(i=0; i<td.length; i++){
-    td[i].onclick = function() {
+    td[i].onclick = function(){
         var input = document.querySelector('input')
-        // console.log(event.target.innerHTML) //value : <input> / innerHTML : text
-        input.value = event.target.innerHTML
-        
-        input.a = "3"
+        input.value += event.target.innerHTML
+    //질문) 원래의 value값인 0이 없어졌으면 좋겠다.
 
-        var obj = {
-            name: '3'
-        }
-        obj.address = 'abcd'
+    var ac = document.querySelector('.ac')
+    ac.addEventListener('click',function(){
+            input.value = "0";
+    })
+    // 위와 같은 말 : ac.onclick = function(){ input.value = "0"; }
+
     }
+    //질문)여기에 넣어도 작동합니다! 어디에 넣어야하나요?
+    // var ac = document.querySelector('.ac')
+    // ac.addEventListener('click',function(){
+    //     // var input = document.querySelector('input')
+    //     // 질문) 변수 input은 'onclikck 함수'안에서 정의된 지역변수이니까 
+    //     // 여기서는 undefined되어야하는것 아닌가용?
+    //         input.value = "0";
+    // })
 }
+//질문)여기에 넣어도 작동합니다! 어디에 넣어야하나요?
+var ac = document.querySelector('.ac')
+    ac.addEventListener('click',function(){
+        var input = document.querySelector('input')
+            input.value = "0";
+    })
+
+
+
