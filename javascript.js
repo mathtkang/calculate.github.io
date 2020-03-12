bdb11d81f4965a30cd3ff17aa9d97f37f20afe7e
mathtkang <51039577+mathtkang@users.noreply.github.com>
2020-03-12 14:36:06 +0900

    계산기 완성
    200310

--- javascript.js
+++ javascript.js
@@ -1,41 +1,41 @@
 var td = document.querySelectorAll('td')
 var input = document.querySelector('input')
+var flag = false
+var resultCheck = false
 for(i=0; i<td.length; i++){
     td[i].onclick = function(e){
-        // 버튼의 내용이 = 이면 기본 동작 취소
-        // this = e.target
-        // this = event.target(타겟자체가 속성)
         if(e.target.innerText == '=') {
-            return
-            //e.preventDefault()
+            return  // 기본동작 취소는 preventDefault! 
+                    // 함수 종료는 return!
         }
-        input.value += e.target.innerHTML
-        //질문) 원래의 value값인 0이 없어졌으면 좋겠다.
-        // 위와 같은 말 
-        // ac.onclick = function(){ input.value = "0"; }
-
-        var b = document.querySelector('.a')
-        var flag = false // 스위치변수 , 플래그변수
-        // 부호를 클릭했다! 그러면 깃발을 올리고 true
-        // 숫자를 클릭했다 그러면 깃발을 내리고 false        
-        // if(e.target.hasClass('a')) {
-            // 숙제: 연산자를 연속으로 클릭하지 못하도록
-        if(isNaN(e.target.innerHTML)) { //isNaN : 숫자가 아니면  true/ false
-            flag = true
-        } else {
+        if(isNaN(e.target.innerText)==false){ //isNaN : 숫자가 아니면 true/ 숫자면 : false 
+            if (input.value == "0" || resultCheck == true){
+                input.value = e.target.innerHTML
+                resultCheck = false
+            }else{
+                input.value += e.target.innerHTML
+            }
             flag = false
+        }else{
+            if(flag == false){
+                input.value += e.target.innerHTML
+                flag = true
+            }        
         }
-        //
     }
 }
 var ac = document.querySelector('.ac')
 ac.addEventListener('click',function(){
     input.value = "0";
 })
+
 var result = document.querySelector('#result')
 result.addEventListener('click',function(){
-    //e.preventDefault()
-    input.value = Number(eval(input.value))
+    if(flag == true){
+        return
+    }
+    input.value = Number(eval(input.value)) //eval() : (안에있는 숫자,부호) 알아서 계산해줌
+    resultCheck = true
     
 })
 
